26f9851273a805039229633fb975d8a7fe8ce0ae
Alexander Sinishin <sinishin@gmail.com>
2020-06-16 07:14:51 -0700

    Update

---
+++ f-canopy/dist/main~df96138e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{H7Ub:function(n,w,o){}}]);
--- f-canopy/dist/vendors~3c1e547b.js
+++ f-canopy/dist/vendors~3c1e547b.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[48],{QfrQ:function(t,e){
+(window.webpackJsonp=window.webpackJsonp||[]).push([[54],{QfrQ:function(t,e){
 /**
  * @license AngularJS v1.7.9
  * (c) 2010-2018 Google, Inc. http://angularjs.org
